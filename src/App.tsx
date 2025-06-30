29c1cdc52f9a561d3d391fcd62322e7c85f7e43f
Waleed Rawasia <middul.dk@gmail.com>
2025-06-30 09:22:54 -0600

    v0.1 release

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { updater } from './utils/updater';
+import { useEffect } from 'react';
 // import Header from './components/Header';
 
 export default function App() {
+
+  useEffect(() => {
+    //runs the updater
+    updater()
+  }, [])
+
   return (
     <BrowserRouter>
       {/* <Header /> */}
