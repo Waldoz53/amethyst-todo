7be861c325921db7b3fe87fa755c74ec72a0a509
Waleed Rawasia <middul.dk@gmail.com>
2025-07-03 14:29:58 -0600

    update to v0.2.1

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,9 @@
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { formatTimeLeft } from '../utils/formatTimeLeft';
+import { useSettingsStore } from '../stores/useSettingsStore';
+import { message } from '@tauri-apps/plugin-dialog';
 
 type TodoItem = {
   text: string;
@@ -24,18 +26,47 @@ export default function TodoItem({
   onToggle,
 }: TodoItemProps) {
   const [timeLeft, setTimeLeft] = useState('');
+  const popupShown = useRef(false)
+
+  const deleteTimer = useRef<ReturnType<typeof setTimeout> | null>(null)
+  const { settings } = useSettingsStore()
+  const autoDelete = settings.autoDeleteTasksOnComplete
+  const autoDeleteTimer = settings.autoDeleteTimer * 1000
 
   useEffect(() => {
-    const update = () => {
+    const update = async () => {
       const due = new Date(item.dueDate).getTime();
       const now = Date.now();
       setTimeLeft(formatTimeLeft(due - now));
+
+      if (settings.enablePopupAfterTaskExpiry && timeLeft == "Overdue!" && !popupShown.current) {
+        popupShown.current = true
+        await message(`${item.text}`, {
+          title: "Task Overdue",
+          kind: 'warning'
+        })
+      }
     };
 
     update();
     const interval = setInterval(update, 1000);
     return () => clearInterval(interval);
   }, [item.dueDate]);
+
+  useEffect(() => {
+    if (autoDelete && item.completed) {
+      deleteTimer.current = setTimeout(() => {
+        onRemove(index)
+      }, autoDeleteTimer)
+    }
+
+    return () => {
+      if (deleteTimer.current) {
+        clearTimeout(deleteTimer.current);
+        deleteTimer.current = null;
+      }
+    }
+  }, [item.completed, autoDelete])
 
   return (
     <div
@@ -53,7 +84,9 @@ export default function TodoItem({
         </strong>
         {item.text}
       </p>
+
       <div className="spacer"></div>
+
       <button
         onClick={(e) => {
           e.stopPropagation();
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -11,10 +11,15 @@ export default function Settings() {
 
   const { settings, updateSettings, saveToFile } = useSettingsStore();
   const [defaultHours, setDefaultHours] = useState(settings.defaultHours);
+  const [autoDelete, setAutoDelete] = useState(settings.autoDeleteTasksOnComplete)
+  const [autoDeleteTimer, setAutoDeleteTimer] = useState(settings.autoDeleteTimer)
+  const [enablePopup, setEnablePopup] = useState(settings.enablePopupAfterTaskExpiry)
 
   useEffect(() => {
     setDefaultHours(settings.defaultHours);
-  }, [settings.defaultHours]);
+    setAutoDelete(settings.autoDeleteTasksOnComplete)
+    setAutoDeleteTimer(settings.autoDeleteTimer)
+  }, [settings]);
 
   const handleCheck = async () => {
     setChecking(true);
@@ -29,23 +34,63 @@ export default function Settings() {
 
   const handleBlur = () => {
     updateSettings("defaultHours", defaultHours);
+    updateSettings("autoDeleteTasksOnComplete", autoDelete)
+    updateSettings("autoDeleteTimer", autoDeleteTimer)
     saveToFile();
   };
+
+  const toggleAutoDelete = () => {
+    const newValue = !autoDelete
+    setAutoDelete(newValue)
+    updateSettings("autoDeleteTasksOnComplete", newValue)
+    saveToFile()
+  }
+
+  const toggleEnablePopup = () => {
+    const newValue = !enablePopup
+    setEnablePopup(newValue)
+    updateSettings("enablePopupAfterTaskExpiry", newValue)
+    saveToFile()
+  }
 
   return (
     <main className="settings">
       <h1>Settings</h1>
+      <p style={{ fontSize: "12px" }}>Settings are automatically saved</p>
 
       <div className="input-container default-hours">
-        <label htmlFor="defaultHours">Default Hours:</label>
+        <label htmlFor="defaultHours">Default Task Time (in hours):</label>
         <input
-          id="defaultHours"
+          name="defaultHours"
           type="number"
           value={defaultHours}
           onChange={(e) => setDefaultHours(parseInt(e.target.value))}
           onBlur={handleBlur}
         />
       </div>
+
+      <div className="input-container">
+        <label htmlFor="autoDelete">Automatically delete completed tasks?</label>
+        <input
+          name="autoDelete"
+          type="checkbox"
+          checked={autoDelete}
+          onChange={() => toggleAutoDelete()}
+        />
+
+        {autoDelete &&
+          <>
+            <label htmlFor="autoDeleteTimer">Auto Delete Timer (in seconds):</label>
+            <input name="autoDeleteTimer" type="number" value={autoDeleteTimer} onChange={(e) => setAutoDeleteTimer(parseInt(e.target.value))} onBlur={handleBlur} />
+          </>
+        }
+      </div>
+
+      <div className="input-container">
+        <label htmlFor="expire-notification">Send an alert if a task's timer expires?</label>
+        <input name="expire-notification" type="checkbox" checked={enablePopup} onChange={() => toggleEnablePopup()} />
+      </div>
+
 
       <button onClick={wipeList}>Delete To Do List Data</button>
       <button onClick={handleCheck}>
@@ -55,7 +100,7 @@ export default function Settings() {
       <div className="information">
         <p>Amethyst To Do List</p>
         <p>Developed by Amethyst Software (Waleed R.)</p>
-        <p>v0.2.0</p>
+        <p>v0.2.1</p>
       </div>
     </main>
   );
